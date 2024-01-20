7d6dd4a3dc915e1c624210f544fcfad937d341d3
Damien Guard <damieng@gmail.com>
2024-01-20 20:47:56 +0000

    Fix END key

--- keyboards/mehkee96/keymaps/damieng/keymap.c
+++ keyboards/mehkee96/keymaps/damieng/keymap.c
@@ -32,7 +32,7 @@ ________________________________________________________________________________
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_P1,   KC_P2,   KC_P3,   KC_PSLS,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_DEL,  KC_UP,   KC_PGUP, KC_PAST,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_RSFT, KC_LEFT, KC_HOME, KC_RGHT, KC_PPLS,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_NO,   KC_RGUI, MO(1),   KC_RCTL, KC_RGHT, KC_DOWN, KC_PGDN, KC_PMNS),
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_NO,   KC_RGUI, MO(1),   KC_RCTL, KC_END,  KC_DOWN, KC_PGDN, KC_PMNS),
 
 
 
