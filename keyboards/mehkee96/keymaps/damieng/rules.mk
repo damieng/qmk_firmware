b5b4f2375d6b0164556ae82a910be9e8d13c7f09
Damien Guard <damien@envytech.co.uk>
2024-01-20 19:01:15 +0000

    Update rules.mk

--- keyboards/mehkee96/keymaps/damieng/rules.mk
+++ keyboards/mehkee96/keymaps/damieng/rules.mk
@@ -1,12 +1,12 @@
 # Build Options
 #   change yes to no to disable
 #
-MOUSEKEY_ENABLE = no	  # Mouse keys
-COMMAND_ENABLE = no     # Commands for debug and configuration
-LTO_ENABLE = yes		    # Smaller binary size
-SLEEP_LED_ENABLE = no   # Stops weird resume problems
-SPACE_CADET_ENABLE = no # No weird open/close parens
-GRAVE_ESC_ENABLE = no   # No grave/esc combo key
+MOUSEKEY_ENABLE = no      # Mouse keys
+COMMAND_ENABLE = no       # Commands for debug and configuration
+LTO_ENABLE = yes          # Smaller binary size
+SLEEP_LED_ENABLE = no     # Stops weird resume problems
+SPACE_CADET_ENABLE = no   # No weird open/close parens
+GRAVE_ESC_ENABLE = no     # No grave/esc combo key
 AVR_USE_MINIMAL_PRINTF = yes # Saves 1.5k
-MAGIC_ENABLE = yes		  # Needed for NKRO
-NKRO_ENABLE = yes				# Needed for NKRO toggle
+MAGIC_ENABLE = yes        # Needed for NKRO
+NKRO_ENABLE = yes         # Needed for NKRO toggle
